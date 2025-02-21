03e1030206ea1cf731fbd30ff187c7e051eb7285
Альберт <gatiatullinalbert9@gmail.com>
2025-02-21 14:38:59 +0300

    Сделал Profile

--- app/src/main/java/com/example/anybreak/profile.java
+++ app/src/main/java/com/example/anybreak/profile.java
@@ -3,6 +3,7 @@ package com.example.anybreak;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -23,12 +24,43 @@ public class profile extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        TextView accountTextView = findViewById(R.id.textView10);
-        accountTextView.setOnClickListener(new View.OnClickListener() {
+        Button editProfileTextTextView = findViewById(R.id.buttoneditprofele);
+        editProfileTextTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(profile.this, editprofile.class);
+                startActivity(intent);
+            }
+        });
+        Button securityTextTextView = findViewById(R.id.buttonsecurity);
+        securityTextTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(profile.this, security.class);
+                startActivity(intent);
+            }
+        });;
+        Button likedTextTextView = findViewById(R.id.buttonliked);
+        likedTextTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(profile.this, liked.class);
+                startActivity(intent);
+            }
+        });
+        Button helpTextTextView = findViewById(R.id.buttonhelp);
+        helpTextTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(profile.this, helpsupport.class);
+                startActivity(intent);
+            }
+        });
+        Button logoutTextTextView = findViewById(R.id.buttonlogout);
+        logoutTextTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(profile.this, registration.class);
                 startActivity(intent);
             }
         });
